c002045734f30cdb8bcdcdfcbc6206f569303999
alfonsocq <alfcastroq@gmail.com>
2022-03-04 19:31:10 -0500

    funciones

--- poco.py
+++ poco.py
@@ -39,12 +39,62 @@ estudiantes1 = [
          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-iterateDictionary(estudiantes1) 
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
 
-def iterateDictionary(some_list)=
+def iterate_dictionary(alfa):
+  for ga in range(0, len(alfa)):
+   resultadofinal2 = ""
+   for a1,a2 in alfa[ga].items():
+    resultadofinal2 += f"{a1}-{a2}"
+   print(resultadofinal2)
+
+iterate_dictionary(estudiantes1)
+    
 
 #Crea una función iterateDictionary(some_list)para que, dada una lista de diccionarios, la función recorra cada diccionarios de la lista e imprima cada llave y el valor asociado. Por ejemplo, dada la siguiente lista:
+
+#3
+estudiantes2 = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary2(codigo_name,beta):
+  for re in range(0, len(beta)):
+      
+   for codigo,bingo in beta[re].items():
+     if codigo == codigo_name:
+       print(bingo)
+
+iterate_dictionary2('first_name',estudiantes2)
+iterate_dictionary2('last_name',estudiantes2)
+    
+
+#4
+
+dojo = {
+   'ubicaciones': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructores': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+
+}
+def PrintInfo(some_direct):
+ for do1,do2 in some_direct.items():
+  print(" ")
+  print(f"{len(do2)}{do1.upper()}")
+  for sha in range (0, len(do2)):
+   print(do2[sha])
+
+PrintInfo(dojo)
+#Error
+##def PrintInfo(some_direct):
+#    for ultima in range (0, len(some_direct)):
+#       resultado3= ""
+#     for c1,c2 in some_direct(ultima).items():
+#        resultado3= len(some_direct) + some_direct() + somedirect[]
+#    print(resultado3)
+#         
+#PrintInfo(dojo)
+
+
+    
